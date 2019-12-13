b8416c873fc9f70c7b52ea8c239842189018d2a7
mahyartajeri <50030069+mahyartajeri@users.noreply.github.com>
2019-12-13 14:15:10 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -16,6 +16,30 @@ public class Hero{
         this.speed = speed;
         this.name = name;
     }
+    //getters and setters
+    public int getHealth(){
+        return this.health;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    public int getDefence(){
+        return this.defence;
+    }
+    public String getName(){
+        return this.name;
+    }
+    
+    //attack method
+    public void attack(Hero hero, int damage){
+        int trueDamage;
+        trueDamage = damage - defence/2;
+        if(trueDamage < 0){
+            trueDamage = 0;
+        }
+        System.out.println(this.name + " did " + trueDamage + " damage to " + hero.getName());
+        hero.setHealth(hero.getHealth() - trueDamage);
+    }
     
    
     
