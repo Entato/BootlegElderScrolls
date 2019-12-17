cbdb135217550d1915b3977cc314cb35f2b297ce
mahyartajeri <50030069+mahyartajeri@users.noreply.github.com>
2019-12-16 23:36:07 -0500

    Update Healer.java

--- Healer.java
+++ Healer.java
@@ -1,13 +1,25 @@
 public class Healer extends Hero{
     //fields
     private int healing;
-    private int manaRegen;
+    private int mana;
     
     //constructor
     public Healer(int health, int defence, int magicResist, int evasion, int speed, String name, int healing, 
-                  int manaRegen){
+                  int mana){
         super(health, defence, magicResist, evasion, speed, name);
         this.healing = healing;
-        this.manaRegen = manaRegen;
+        this.mana = mana;
+    }
+
+    //get item
+    public void useItem(Item item){
+        super.useItem(item);
+
+        if(item.getItemType() == Item.ItemType.ATTACK){
+            healing += 50;
+        }
+        else if(item.getItemType() == Item.ItemType.ATTACK){
+            mana += 50;
+        }
     }
 }
