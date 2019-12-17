4a60b43c6dd2303da143d38236d36c0a4b3bda67
Kai <kaichengxu12@hotmail.com>
2019-12-17 16:33:08 -0500

    fixed parameters for heros in main

--- Battle.java
+++ Battle.java
@@ -2,12 +2,12 @@ import java.util.Random;
 
 class Battle{
     public static void main(String[] args){
-        Hero hero1 = new Wizard(100, 0, 0, 0, 0, "wizzyboy", 2, 3);
+        Hero hero1 = new Wizard("wizzyboy");
         Hero hero2 = new Knight("REEE");
-        Hero hero3 = new Healer(100, 0, 0, 0, 0, "healer", 0, 0);
+        Hero hero3 = new Healer("healer");
 
-        Hero hero4 = new Assassin(100, 0, 0, 0, 0, "ass ass in", 2);
-        Hero hero5 = new Archer(100, 0, 0, 0, 0, "aRcHEr", 5);
+        Hero hero4 = new Assassin("ass ass in");
+        Hero hero5 = new Archer("aRcHEr");
         Hero hero6 = new Knight("name");
 
         Hero[] team1 = {hero1, hero2, hero3};
