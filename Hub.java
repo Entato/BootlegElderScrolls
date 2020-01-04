e8f9478c6cb58d1a2da5058a6dce6ded1a193523
Kai <kaichengxu12@hotmail.com>
2020-01-04 16:29:51 -0500

    prototype hub

--- Hub.java
+++ Hub.java
@@ -1,6 +1,5 @@
 package BootlegElderScrolls;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,6 +8,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 class Hub{
+    //GUI for the hub
     public static Scene hubScene(Stage stage){
         BorderPane borderPane = new BorderPane();
 
@@ -17,15 +17,23 @@ class Hub{
         Label party = new Label("Party:");
         partyList.getChildren().add(party);
 
+        //adds all the party member to a vBox
+        for (int i = 0; i < Game.getTeam1().size(); i++){
+            Label member = new Label(Game.getTeam1().get(i).getName());
+            partyList.getChildren().add(member);
+        }
+
+        //button to start battle
         Button startBattle = new Button("Battle");
         startBattle.setOnAction(e -> {
             stage.setScene(Battle.createBattleScene());
         });
 
+        //adds everything to borderPane
         borderPane.setLeft(partyList);
         borderPane.setBottom(startBattle);
 
-        Scene hub = new Scene(borderPane);
+        Scene hub = new Scene(borderPane, 1000, 650);
         return hub;
     }
 }
