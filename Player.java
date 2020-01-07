c5f9350670ac4bff849f0576b5dc2e9905db137c
Kai <kaichengxu12@hotmail.com>
2020-01-06 22:03:09 -0500

    added getters and setters to player.java

--- Player.java
+++ Player.java
@@ -8,10 +8,10 @@ class Player {
     private static ArrayList<Hero> playerTeam = new ArrayList<Hero>();
     private static ArrayList<ComboBox> heroSelections = new ArrayList<ComboBox>();
     private static ArrayList<TextField> heroNames = new ArrayList<TextField>();
-    private int score;
-    private int bossCount;
-    private int totalDamage;
-    private int kills;
+    private static int score = 0;
+    private static int bossCount = 0;
+    private static int totalDamage = 0;
+    private static int kills = 0;
 
     public static ArrayList<Hero> getPlayerTeam(){
         return playerTeam;
@@ -23,5 +23,37 @@ class Player {
 
     public static ArrayList<TextField> getHeroNames(){
         return heroNames;
+    }
+
+    public static void scoreAdd (int score){
+        Player.score += score;
+    }
+
+    public static void bossCountAdd(){
+        bossCount++;
+    }
+
+    public static void totalDamageAdd(int damage){
+        totalDamage += damage;
+    }
+
+    public static void killsAdd(){
+        kills++;
+    }
+
+    public static int getScore(){
+        return score;
+    }
+
+    public static int getBossCount(){
+        return bossCount;
+    }
+
+    public static int getTotalDamage(){
+        return totalDamage;
+    }
+
+    public static int getKills(){
+        return kills;
     }
 }
