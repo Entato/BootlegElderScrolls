48f053751749230dbb82f46be954c12d5a946019
Kai <kaichengxu12@hotmail.com>
2020-01-12 22:46:44 -0500

    nerfed grunts to make it easier to get to later stages for testing

--- Grunt.java
+++ Grunt.java
@@ -3,6 +3,6 @@ package BootlegElderScrolls;
 public class Grunt extends Boss{
     //constructor
     public Grunt(String name, int Stage){
-        super(50 + Stage*25, 75 + Stage*25, 25, 25, 10, 75, name);
+        super(35 + Stage*15, 75 + Stage*15, 25 + Stage*5, 25, 10, 25 + Stage*10, name);
     }
 }
