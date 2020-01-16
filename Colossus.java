86aa1ab60bf6715f71369382f5ec1e6fbad63407
unknown <mahyar.tajeri@gmail.com>
2020-01-15 22:30:43 -0500

    boss stats

--- Colossus.java
+++ Colossus.java
@@ -2,6 +2,6 @@ package BootlegElderScrolls;
 
 public class Colossus extends Boss{
     public Colossus (){
-        super(250, 600, 300, 25, 5, 50, "Colossus");
+        super(250, 600, 250, 25, 5, 50, "Colossus");
     }
 }
