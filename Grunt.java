20e065b96498349537a9da2e277aedb9fffe55ee
Kai <kaichengxu12@hotmail.com>
2020-01-16 23:51:29 -0500

    balanced grunts

--- Grunt.java
+++ Grunt.java
@@ -3,6 +3,6 @@ package BootlegElderScrolls;
 public class Grunt extends Boss{
     //constructor
     public Grunt(String name, int Stage){
-        super(45 + Stage*15, 85 + Stage*15, 50 + Stage*5, 10, 25 + Stage*10, name);
+        super(70 + Stage*15, 125 + Stage*15, 50 + Stage*5, 5, 45 + Stage*10, name);
     }
 }
